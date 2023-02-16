936bc2796badd15492d98756dc5037db49e50104
Steve <119956563+sjbeli@users.noreply.github.com>
2023-02-16 02:07:55 +0000

    complete html-css add initial js

---
+++ all-sports-score/index.js
@@ -0,0 +1,18 @@
+let score = document.getElementsByClassName("score")
+
+// initializes / resets scores to 0
+start()
+
+
+
+function start() {
+    score[0].innerText = 0
+    score[1].innerText = 0
+}
+console.log(score)
+
+
+function addOne() {
+    // get current score & add 1
+
+}
