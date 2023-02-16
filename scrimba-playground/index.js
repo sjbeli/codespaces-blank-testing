321817c8d2958f5d89ca77bfd7725d7d21d98239
Steve <119956563+sjbeli@users.noreply.github.com>
2023-02-16 00:09:52 +0000

    complete html css

---
+++ scrimba-playground/index.js
@@ -0,0 +1,13 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+
+let firstName = "Steve"
+let lastName = "Beliveau"
+
+let fullName = firstName + lastName
+
+console.log(fullName)
